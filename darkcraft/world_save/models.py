07fe9fef16a864b7ee0a2ad1302e4605cfa8b3e4
Kelly Ryan <kellyryan@Kellys-MacBook-Pro.local>
2014-11-21 10:23:36 -0500

    added new file store

--- darkcraft/world_save/models.py
+++ darkcraft/world_save/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+from django.core.files.storage import FileSystemStorage
+
+class LocalFileStorage(FileSystemStorage):
+
+    # This method is actually defined in Storage
+    def get_available_name(self, name):
+      return name # simply returns the name passed
+
 class WorldSave(models.Model):
     date = models.DateTimeField(blank=True)
     signature = models.CharField(max_length=255)
-    compressed_file = models.FileField(blank=True,null=True)
+    compressed_file = models.FileField(blank=True,null=True, storage=LocalFileStorage())
     ip = models.CharField(max_length=255,blank=True)
 
     def save(self,*args, **kwargs):
